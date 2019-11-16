18ec28ed654f7c53d93466996088b5691933f0f7
jeferson rocha <jeferson.rocha8@gmail.com>
2019-11-16 13:13:52 -0300

    add botao interativo ao mapa

--- lib/Home.dart
+++ lib/Home.dart
@@ -11,20 +11,47 @@ class _HomeState extends State<Home> {
 
   Completer<GoogleMapController> _controller = Completer();
 
+  _onMapCreated(GoogleMapController googleMapController){
+
+   _controller.complete( googleMapController );
+
+  }
+
+  _movimentarCamera()async{
+
+   GoogleMapController googleMapController = await _controller.future;
+   googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(-30.086764, -51.045771),
+          zoom: 16,
+          tilt: 0,
+          bearing: 30
+        )
+      )
+   );
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Mapas e geolocalização"),),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.done),
+          onPressed: _movimentarCamera,
+      ),
       body: Container(
         child: GoogleMap(
-          mapType: MapType.normal,
+          //mapType: MapType.satellite,
+            mapType: MapType.normal,
             initialCameraPosition: CameraPosition(
-                target: LatLng(-23.562436, -46.655005),
+                target: LatLng(-30.086764, -51.045771),
               zoom: 16
             ),
-          onMapCreated: (GoogleMapController controller){
-            _controller.complete(controller);
-          },
+          onMapCreated: _onMapCreated,
         ),
       ),
     );
